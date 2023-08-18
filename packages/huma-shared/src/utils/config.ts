0b8fd978a9a61898109e9647e1157baa8590f703
shan <shan@57blocks.com>
2023-08-18 11:32:14 +0800

    update git hooks

--- packages/huma-shared/src/utils/config.ts
+++ packages/huma-shared/src/utils/config.ts
@@ -2,11 +2,11 @@ import { CHAINS } from './chain'
 
 const getDevPrefix = (isDev = false) => (isDev ? 'dev.' : '')
 
-const getNetworkType = (chainId: number) => CHAINS[chainId].isTestnet ? 'testnet' : 'mainnet'
+const getNetworkType = (chainId: number) =>
+  CHAINS[chainId].isTestnet ? 'testnet' : 'mainnet'
 
-const getNetworkAgnosticServiceUrlPrefix = (chainId: number, isDev: boolean) => `${getDevPrefix(
-    isDev,
-  )}${getNetworkType(chainId)}`
+const getNetworkAgnosticServiceUrlPrefix = (chainId: number, isDev: boolean) =>
+  `${getDevPrefix(isDev)}${getNetworkType(chainId)}`
 
 const getEAVerseUrl = (chainId: number, isDev = false) => {
   const network = CHAINS[chainId].name
@@ -29,9 +29,17 @@ const getRequestAPIUrl = (chainId: number, isDev = false) => {
   return `https://${getDevPrefix(isDev)}${network}.rnreader.huma.finance`
 }
 
-const getIdentityAPIUrl = (chainId: number, isDev = false) => `https://${getNetworkAgnosticServiceUrlPrefix(chainId, isDev)}.identity-verification.huma.finance`
+const getIdentityAPIUrl = (chainId: number, isDev = false) =>
+  `https://${getNetworkAgnosticServiceUrlPrefix(
+    chainId,
+    isDev,
+  )}.identity-verification.huma.finance`
 
-const getAuthServiceUrl = (chainId: number, isDev = false) => `https://${getNetworkAgnosticServiceUrlPrefix(chainId, isDev)}.auth.huma.finance`
+const getAuthServiceUrl = (chainId: number, isDev = false) =>
+  `https://${getNetworkAgnosticServiceUrlPrefix(
+    chainId,
+    isDev,
+  )}.auth.huma.finance`
 
 const getKYCProviderBaseUrl = (provider: 'Securitize', chainId: number) => {
   switch (provider) {
