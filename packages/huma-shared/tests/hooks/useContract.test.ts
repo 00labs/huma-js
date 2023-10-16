42c24a9850c2c3f42e31bbfde8bfffaf86b287fb
Michael Liu <michael.liu95@gmail.com>
2023-10-16 16:49:44 -0400

    Update tests

--- packages/huma-shared/tests/hooks/useContract.test.ts
+++ packages/huma-shared/tests/hooks/useContract.test.ts
@@ -1,13 +1,15 @@
 import { Contract } from '@ethersproject/contracts'
 import { renderHook } from '@testing-library/react'
 import { useWeb3React } from '@web3-react/core'
-
+import { JsonRpcProvider } from '@ethersproject/providers'
 import { useContract, useMultiSendContract } from '../../src/hooks/useContract'
 import { getContract } from '../../src/utils/web3'
 
 jest.mock('@web3-react/core', () => ({
   useWeb3React: jest.fn(),
 }))
+
+jest.mock('@ethersproject/providers')
 
 jest.mock('../../src/utils/web3', () => ({
   getContract: jest.fn(),
@@ -51,15 +53,76 @@ describe('useContract', () => {
     expect(result.current).toBeNull()
   })
 
-  it('returns null if chainId is not provided', () => {
+  it('returns null if desiredChainId is not the same as connected chain and fallbackProviders is null', () => {
     ;(useWeb3React as jest.Mock).mockReturnValue({
       provider: 'provider',
-      chainId: null,
+      chainId: 3,
     })
 
-    const { result } = renderHook(() => useContract('0x123', []))
+    const { result } = renderHook(() => useContract('0x123', [], true, 1))
 
     expect(result.current).toBeNull()
+  })
+
+  it('returns the contract using fallbackProvider if no provider connected', () => {
+    const mockContract = new Contract(
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      [],
+    )
+    ;(useWeb3React as jest.Mock).mockReturnValue({
+      provider: null,
+    })
+    ;(getContract as jest.Mock).mockReturnValue(mockContract)
+
+    const { result } = renderHook(() =>
+      useContract('0x123', [], true, 3, { 3: '3 chain fallback' }),
+    )
+
+    expect(result.current).toEqual(mockContract)
+    expect(JsonRpcProvider).toHaveBeenCalledWith('3 chain fallback')
+  })
+
+  it('returns the contract using fallbackProvider if provider chain is not desiredChain', () => {
+    const mockContract = new Contract(
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      [],
+    )
+    ;(useWeb3React as jest.Mock).mockReturnValue({
+      provider: 'provider',
+      chainId: 1,
+    })
+    ;(getContract as jest.Mock).mockReturnValue(mockContract)
+
+    const { result } = renderHook(() =>
+      useContract('0x123', [], true, 3, { 3: '3 chain fallbakc' }),
+    )
+
+    expect(result.current).toEqual(mockContract)
+    expect(JsonRpcProvider).toHaveBeenCalledWith('3 chain fallback')
+  })
+
+  it('returns the contract using provider if connected chain matches desiredChain, even if fallbackProviders is given', () => {
+    const mockContract = new Contract(
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      [],
+    )
+    ;(useWeb3React as jest.Mock).mockReturnValue({
+      provider: 'provider',
+      chainId: 3,
+    })
+    ;(getContract as jest.Mock).mockReturnValue(mockContract)
+
+    const { result } = renderHook(() =>
+      useContract('0x123', [], true, 3, { 3: 'fallback' }),
+    )
+
+    expect(result.current).toEqual(mockContract)
+    expect(getContract).toBeCalledWith(
+      expect.anything(),
+      expect.anything(),
+      'provider',
+      undefined,
+    )
   })
 
   it('returns the contract if all parameters are provided', () => {
@@ -68,17 +131,23 @@ describe('useContract', () => {
       [],
     )
     ;(useWeb3React as jest.Mock).mockReturnValue({
-      provider: {},
+      provider: 'provider',
       account: '0x0',
       chainId: 1,
     })
     ;(getContract as jest.Mock).mockReturnValue(mockContract)
 
     const { result } = renderHook(() =>
-      useContract('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', [''], true),
+      useContract('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', [], true),
     )
 
     expect(result.current).toEqual(mockContract)
+    expect(getContract).toBeCalledWith(
+      expect.anything(),
+      expect.anything(),
+      'provider',
+      '0x0',
+    )
   })
 
   it('returns null if getContract throws an error', () => {
