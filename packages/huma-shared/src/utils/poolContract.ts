832c9e1463ca16b7bd29b8f6ac28cfdd9b89a0a3
shan <shan@57blocks.com>
2023-12-08 17:40:21 +0800

    add getLenderPosition

--- packages/huma-shared/src/utils/poolContract.ts
+++ packages/huma-shared/src/utils/poolContract.ts
@@ -1,7 +1,9 @@
 import { JsonRpcProvider, Web3Provider } from '@ethersproject/providers'
+import { BigNumber } from 'ethers'
 
-import { BasePoolConfig } from '../abis/types'
+import { BasePoolConfig, HDT } from '../abis/types'
 import { getChainIdFromSignerOrProvider } from './chain'
+import { upScale } from './number'
 import { getPoolInfo, POOL_NAME, POOL_TYPE } from './pool'
 import { getContract } from './web3'
 
@@ -26,4 +28,38 @@ export async function getPoolApr(
 
   const aprInBps = await contract.poolAprInBps()
   return aprInBps.toNumber() / 10000
+}
+
+export async function getLenderPosition(
+  poolName: POOL_NAME,
+  poolType: POOL_TYPE,
+  account: string | undefined,
+  provider: JsonRpcProvider | Web3Provider | undefined,
+): Promise<BigNumber | undefined> {
+  if (!account) {
+    return undefined
+  }
+  const chainId = await getChainIdFromSignerOrProvider(provider)
+  const poolInfo = getPoolInfo(chainId, poolName, poolType)
+  if (!poolInfo) {
+    return undefined
+  }
+  const contract = getContract<HDT>(
+    poolInfo.HDT.address,
+    poolInfo.HDT.abi,
+    provider,
+  )
+  if (!contract) {
+    return undefined
+  }
+
+  const position = await contract.withdrawableFundsOf(account)
+  const oneCentUSD = BigNumber.from(
+    upScale('0.01', poolInfo.poolUnderlyingToken.decimals),
+  )
+  // if position is less than one cent USD, consider it 0
+  if (position.lt(oneCentUSD)) {
+    return BigNumber.from(0)
+  }
+  return position
 }
