dfaca484fe1b601b7d1162e6148cc2a40fc69eee
shan <shan@57blocks.com>
2024-06-12 16:50:53 +0800

    update tests

--- packages/huma-shared/tests/v2/utils/pool.test.ts
+++ packages/huma-shared/tests/v2/utils/pool.test.ts
@@ -5,30 +5,14 @@ import {
   getPoolInfoForPoolAddressV2,
 } from '../../../src/v2/utils'
 
-jest.mock('../../../src/v2/metadata/localhost', () => ({
-  LOCALHOST_METADATA: {
+jest.mock('../../../src/v2/metadata/celo', () => ({
+  CELO_METADATA: {
     JiaV2: {
       poolName: 'JiaV2',
       pool: '0x1fA02b2d6A771842690194Cf62D91bdd92BfE28d',
       estAPY: '10-20%',
       underlyingToken: {
         address: '0x1429859428C0aBc9C2C47C8Ee9FBaf82cFA0F20f',
-        symbol: 'USDC',
-        decimals: 18,
-        icon: 'USDC',
-      },
-    },
-  },
-}))
-
-jest.mock('../../../src/v2/metadata/mumbai', () => ({
-  MUMBAI_METADATA: {
-    JiaV2: {
-      poolName: 'JiaV2',
-      pool: '0x3Dd5829A0A20229a18553AAf09415E6139EbC5b9',
-      estAPY: '10-20%',
-      underlyingToken: {
-        address: '0x6Dfb932F9fDd38E4B3D2f6AAB0581a05a267C13C',
         symbol: 'USDC',
         decimals: 18,
         icon: 'USDC',
@@ -41,23 +25,12 @@ describe('getPoolsInfoV2', () => {
   it('should return the poolsInfoV2 object', () => {
     const result = CHAIN_POOLS_INFO_V2
 
-    expect(result[ChainEnum.Localhost].JiaV2).toEqual({
+    expect(result[ChainEnum.Celo].JiaV2).toEqual({
       poolName: 'JiaV2',
       pool: '0x1fA02b2d6A771842690194Cf62D91bdd92BfE28d',
       estAPY: '10-20%',
       underlyingToken: {
         address: '0x1429859428C0aBc9C2C47C8Ee9FBaf82cFA0F20f',
-        symbol: 'USDC',
-        decimals: 18,
-        icon: 'USDC',
-      },
-    })
-    expect(result[ChainEnum.Mumbai].JiaV2).toEqual({
-      poolName: 'JiaV2',
-      pool: '0x3Dd5829A0A20229a18553AAf09415E6139EbC5b9',
-      estAPY: '10-20%',
-      underlyingToken: {
-        address: '0x6Dfb932F9fDd38E4B3D2f6AAB0581a05a267C13C',
         symbol: 'USDC',
         decimals: 18,
         icon: 'USDC',
@@ -80,7 +53,7 @@ describe('getChainPoolNamesV2', () => {
   })
 
   it('should return an array of pool names for valid chainId', () => {
-    const result = getChainPoolNamesV2(ChainEnum.Mumbai)
+    const result = getChainPoolNamesV2(ChainEnum.Celo)
 
     expect(result).toEqual(['JiaV2'])
   })
@@ -88,7 +61,7 @@ describe('getChainPoolNamesV2', () => {
 
 describe('getPoolInfoForPoolAddressV2', () => {
   it('should return null if chainId not found', () => {
-    const chainId = -1
+    const chainId = -1 as ChainEnum
     const poolAddress = '0xa890Ac3c9F8E38Be9c05BFfc0E4ECa21Bbc2FfA9'
 
     expect(getPoolInfoForPoolAddressV2(chainId, poolAddress)).toBe(null)
@@ -102,8 +75,8 @@ describe('getPoolInfoForPoolAddressV2', () => {
   })
 
   it('should return poolInfo', () => {
-    const chainId = ChainEnum.Mumbai
-    const poolAddress = '0x3Dd5829A0A20229a18553AAf09415E6139EbC5b9'
+    const chainId = ChainEnum.Celo
+    const poolAddress = '0x1fA02b2d6A771842690194Cf62D91bdd92BfE28d'
 
     expect(getPoolInfoForPoolAddressV2(chainId, poolAddress)?.poolName).toBe(
       POOL_NAME.JiaV2,
