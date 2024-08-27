6893a81e2bc72df51aacf17bc48390f6fa86864f
shan <shan@57blocks.com>
2024-08-27 18:19:22 +0800

    add scroll pool

--- packages/huma-shared/src/v2/metadata/scroll.ts
+++ packages/huma-shared/src/v2/metadata/scroll.ts
@@ -4,7 +4,7 @@ import { FirstLossCoverIndex } from '../types'
 import { ARF_PERSONA_KYC_COPY, PoolsInfoV2 } from '../utils'
 
 export const SCROLL_METADATA: PoolsInfoV2 = {
-  ArfCreditPoolV2: {
+  ArfCreditPool3Months: {
     chainId: ChainEnum.Scroll,
     poolVersion: 'v2',
     industry: 'Remittance Financing',
@@ -31,13 +31,43 @@ export const SCROLL_METADATA: PoolsInfoV2 = {
     },
     seniorAPY: '10-20%',
     juniorAPY: '10-20%',
-    title: 'Arf Credit Pool v2',
+    title: 'Arf - Cross Border Payment Financing',
     desc: 'Arf provides an innovative on-chain liquidity solution that simplifies cross-border payments by facilitating immediate USDC-based settlements and tokenizing payment orders, enhancing transparency in the process.',
     KYC: {
       Persona: ARF_PERSONA_KYC_COPY,
     },
-    extra: {
-      enableGetTestUSDC: true,
+  },
+  ArfCreditPool6Months: {
+    chainId: ChainEnum.Scroll,
+    poolVersion: 'v2',
+    industry: 'Remittance Financing',
+    poolName: POOL_NAME.ArfCreditPoolV2,
+    poolType: POOL_TYPE.CreditLine,
+    pool: '0x1900e417869691277cfd20f4001b768B03375272',
+    poolConfig: '0xE9D7893B52BE6D81C540f2E81f3A65FAbF8A0fa4',
+    poolCredit: '0x2e3A03C8Bd31300C2c027C9C9d1b762677F6FaA6',
+    poolCreditManager: '0xbC015F64b023d8B351484342d35dA0AF0d42a9de',
+    poolSafe: '0xD6C357c40731a1F57173231eeeAb004eD8baE9b6',
+    seniorTrancheVault: '0xDe5eD603A376B93817b9656AF6E373218fD9b2bd',
+    juniorTrancheVault: '0x8413a7345cD8bF8Afe8c2EfE866a764A93B305e2',
+    epochManager: '0x648186Cf2a78AB9356C82ec29EDA42798453C8d6',
+    poolUnderlyingToken: {
+      address: '0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4',
+      symbol: 'USDC',
+      decimals: 6,
+      icon: 'USDC',
+    },
+    firstLossCovers: {
+      [FirstLossCoverIndex.borrower]:
+        '0xA2A7851D62aEe4dE259b15C11af961bb50a2147e',
+      [FirstLossCoverIndex.admin]: '0x5BaBd32EEb522b891911ce9C184aaEC65A5CEBE9',
+    },
+    seniorAPY: '10-20%',
+    juniorAPY: '10-20%',
+    title: 'Arf - Cross Border Payment Financing',
+    desc: 'Arf provides an innovative on-chain liquidity solution that simplifies cross-border payments by facilitating immediate USDC-based settlements and tokenizing payment orders, enhancing transparency in the process.',
+    KYC: {
+      Persona: ARF_PERSONA_KYC_COPY,
     },
   },
 }
