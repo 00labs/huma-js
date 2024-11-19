98f05183a405a868e880e8910d8fdee071fecfef
Michael Liu <michael.liu95@gmail.com>
2024-11-19 10:45:38 -0500

    Add optimization support for solana txs from frontend (#367)

--- packages/huma-sdk/src/utils/solana/buildOptimalTransaction.ts
+++ packages/huma-sdk/src/utils/solana/buildOptimalTransaction.ts
@@ -1,21 +1,36 @@
 import {
   ComputeBudgetProgram,
+  Connection,
   PublicKey,
   RecentPrioritizationFees,
   Transaction,
 } from '@solana/web3.js'
 import { HumaSolanaContext } from '../../helpers'
 
-export async function buildOptimalTransaction(
+export function extractWritableAccounts(tx: Transaction): PublicKey[] {
+  const writableAccounts = new Set<PublicKey>()
+
+  tx.instructions.forEach((ix) => {
+    ix.keys.forEach((key) => {
+      if (key.isWritable) {
+        writableAccounts.add(key.pubkey)
+      }
+    })
+  })
+
+  return Array.from(writableAccounts)
+}
+
+async function buildOptimalTransactionImpl(
   tx: Transaction,
   txAccounts: PublicKey[],
-  context: HumaSolanaContext,
+  connection: Connection,
 ): Promise<Transaction> {
   const [recentPrioritizationFees, recentBlockhash] = await Promise.all([
-    context.connection.getRecentPrioritizationFees({
+    connection.getRecentPrioritizationFees({
       lockedWritableAccounts: txAccounts,
     }),
-    context.connection.getLatestBlockhash('confirmed'),
+    connection.getLatestBlockhash('confirmed'),
   ])
 
   let averagePrioritizationFee = recentPrioritizationFees.reduce(
@@ -33,5 +48,22 @@ export async function buildOptimalTransaction(
   )
 
   tx.recentBlockhash = recentBlockhash.blockhash
+  tx.lastValidBlockHeight = recentBlockhash.lastValidBlockHeight
   return tx
+}
+
+export async function buildOptimalTransaction(
+  tx: Transaction,
+  txAccounts: PublicKey[],
+  context: HumaSolanaContext,
+): Promise<Transaction> {
+  return buildOptimalTransactionImpl(tx, txAccounts, context.connection)
+}
+
+export async function buildOptimalTransactionFromConnection(
+  tx: Transaction,
+  txAccounts: PublicKey[],
+  connection: Connection,
+): Promise<Transaction> {
+  return buildOptimalTransactionImpl(tx, txAccounts, connection)
 }
