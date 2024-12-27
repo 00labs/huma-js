ff8fb2cae055f9ff151328c47146d43a41431270
shan <shan@57blocks.com>
2024-12-27 16:45:30 +0800

    solana withdraw

---
+++ packages/huma-widget/src/components/Lend/solanaWithdraw/2-Transfer.tsx
@@ -0,0 +1,47 @@
+import { PoolInfoV2, TrancheType } from '@huma-finance/shared'
+import { useTrancheVaultContractV2 } from '@huma-finance/web-shared'
+import { useWeb3React } from '@web3-react/core'
+import React, { useCallback } from 'react'
+
+import { useAppDispatch } from '../../../hooks/useRedux'
+import { setStep } from '../../../store/widgets.reducers'
+import { WIDGET_STEP } from '../../../store/widgets.store'
+import { TxSendModalV2 } from '../../TxSendModalV2'
+
+type Props = {
+  poolInfo: PoolInfoV2
+  tranche: TrancheType
+  poolIsClosed: boolean
+}
+
+export function Transfer({
+  poolInfo,
+  tranche,
+  poolIsClosed,
+}: Props): React.ReactElement | null {
+  const dispatch = useAppDispatch()
+  const { account, provider } = useWeb3React()
+  const trancheVaultContract = useTrancheVaultContractV2(
+    poolInfo.poolName,
+    tranche,
+    provider,
+    account,
+  )
+
+  const handleSuccess = useCallback(() => {
+    dispatch(setStep(WIDGET_STEP.Done))
+  }, [dispatch])
+
+  if (!trancheVaultContract || !account) {
+    return null
+  }
+
+  return (
+    <TxSendModalV2
+      contract={trancheVaultContract}
+      method={poolIsClosed ? 'withdrawAfterPoolClosure' : 'disburse'}
+      params={[]}
+      handleSuccess={handleSuccess}
+    />
+  )
+}
