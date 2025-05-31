20cbd8f338f9a9fedbbcfaf7ecb4679c8a177b79
Michael Liu <michael@huma.finance>
2025-05-31 18:19:06 -0400

    Force dev false

--- packages/huma-web-shared/src/utils/checkIsDev.ts
+++ packages/huma-web-shared/src/utils/checkIsDev.ts
@@ -1,8 +1,1 @@
-export const checkIsDev = () =>
-  import.meta.env.VITE_FORCE_IS_DEV_FALSE !== 'true' &&
-  !!(
-    window.location.hostname.startsWith('dev') ||
-    window.location.hostname.startsWith('pr-') ||
-    window.location.hostname.startsWith('localhost') ||
-    window.location.hostname.startsWith('testnet')
-  )
+export const checkIsDev = () => false
